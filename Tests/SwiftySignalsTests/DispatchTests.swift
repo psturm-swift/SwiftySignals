096ad4f90b6a44cad44af99f6d2a167d31563c5a
Patrick Sturm <patrick.sturm@mail.de>
2017-03-01 07:07:50 +0100

    [refac] Split up one test file into two

--- Tests/SwiftySignalsTests/DispatchTests.swift
+++ Tests/SwiftySignalsTests/DispatchTests.swift
@@ -21,63 +21,58 @@
 import XCTest
 @testable import SwiftySignals
 
-class PropertyTests: XCTestCase {
+class DispatchTests: XCTestCase {
     override func setUp() {
     }
     
     override func tearDown() {
     }
     
-    func testThatPropertyValueCanBeReadOut() {
-        let property = Property<Int>(value: 54)
-        XCTAssertEqual(54, property.value)
-    }
-    
-    func testThatPropertyValueCanBeChanged() {
-        let property = Property<Int>(value: 54)
-        property.value = 64
-        XCTAssertEqual(64, property.value)
-    }
-    
-    func testIfOnlyEvenNumbersCanBeObserved() {
+    func testIfBlocksAreExecutedOnMainQueueOnSubscription() {
         let observables = ObservableCollection()
-        let property = Property<Int>(value: 53)
-
-        let expectEvenNumber = self.expectation(description: "Expect even number")
+        let propertyProcessed = self.expectation(description: "Property processed")
+        let property = Property<Bool>(value: true)
         
         property
             .didSet
-            .filter { $0 % 2 == 0 }
-            .then { XCTAssertTrue($0 % 2 == 0) }
-            .then { _ in expectEvenNumber.fulfill() }
-            .append(to: observables)
-        
-        property.value = 54
-        
-        self.waitForExpectations(timeout: 10, handler: nil)
-    }
-    
-    func testIfLastMessageIsPropagatedOnSubscription() {
-        let observables = ObservableCollection()
-        let property = Property<Int>(value: 13)
-        let expectLastMessage = self.expectation(description: "Expect last message")
-        
-        property
-            .didSet
-            .map { $0 == 13 }
-            .then { XCTAssertTrue($0) }
-            .then { _ in expectLastMessage.fulfill() }
+            .then { _ in XCTAssertTrue(Thread.isMainThread) }
+            .map {
+                value -> Bool in
+                XCTAssertTrue(Thread.isMainThread)
+                return !value
+            }
+            .then { _ in propertyProcessed.fulfill() }
             .append(to: observables)
         
         self.waitForExpectations(timeout: 10, handler: nil)
     }
-
-    static var allTests : [(String, (PropertyTests) -> () throws -> Void)] {
-        let unitTests : [(String, (PropertyTests) -> () throws -> Void)] = [
-            ("testThatPropertyValueCanBeReadOut", testThatPropertyValueCanBeReadOut),
-            ("testThatPropertyValueCanBeChanged", testThatPropertyValueCanBeChanged),
-            ("testIfOnlyEvenNumbersCanBeObserved", testIfOnlyEvenNumbersCanBeObserved),
-            ("testIfLastMessageIsPropagatedOnSubscription", testIfLastMessageIsPropagatedOnSubscription),
+    
+    func testIfBlocksAreExecutedOnMainQueueIfPropertyDidSet() {
+        let observables = ObservableCollection()
+        let propertyProcessed = self.expectation(description: "Property processed")
+        let property = Property<Bool>(value: true)
+        
+        property
+            .didSet
+            .discard(first: 1)
+            .then { _ in XCTAssertTrue(Thread.isMainThread) }
+            .map {
+                value -> Bool in
+                XCTAssertTrue(Thread.isMainThread)
+                return !value
+            }
+            .then { _ in propertyProcessed.fulfill() }
+            .append(to: observables)
+        
+        property.value = !property.value
+        
+        self.waitForExpectations(timeout: 10, handler: nil)
+    }
+    
+    static var allTests : [(String, (DispatchTests) -> () throws -> Void)] {
+        let unitTests : [(String, (DispatchTests) -> () throws -> Void)] = [
+            ("testIfBlocksAreExecutedOnMainQueueOnSubscription", testIfBlocksAreExecutedOnMainQueueOnSubscription),
+            ("testIfBlocksAreExecutedOnMainQueueIfPropertyDidSet", testIfBlocksAreExecutedOnMainQueueIfPropertyDidSet)
         ]
         return unitTests
     }
